9fc54f952a9cfc71395bce7e5f3f261dc4b615f6
DeeshaGohel <goheldeesha@gmail.com>
2025-02-09 11:49:50 +0530

    Home page updated

--- Client/src/Components/Users/Home.jsx
+++ Client/src/Components/Users/Home.jsx
@@ -1,21 +1,45 @@
 import React from "react";
 import Header from "./Header";
-
 const Home = () => {
   return (
-<<<<<<< HEAD:src/Components/Users/Home.jsx
     <>
-    <Header />
-    <div className="flex justify-center bg-[#000000] text-white">
-      Home
-    </div>
+      <Header />
+      <div class="bg-black text-white">
+        <a href="#" class="block shadow-sm ">
+          <img
+            src="banner.jpeg"
+            alt="banner"
+            class="h-auto w-full object-cover rounded-b-[10vw] "
+          />
+        </a>
+        <h1 className="py-2 text-4xl flex justify-center">Overview</h1>
+        <h2 className="mx-6 sm:mx-3 pb-4 text-2xl text-center">
+          Gavali Group of Business is a diversified investment firm specializing
+          in stock trading and real estate, offering comprehensive wealth
+          management solutions. With expertise in equity markets and property
+          investments, we help individuals, corporations, and institutions
+          maximize returns and diversify portfolios.
+        </h2>
+        <div class="bg-gray-800 text-white p-5 mx-6 rounded-lg shadow-lg">
+          <div class="block sm:flex sm:items-center sm:space-x-6">
+          <img
+              src="Logo.png"
+              alt="logo"
+              class="h-24 w-auto sm:h-28 sm:w-full object-contain"
+            />
+            <h1 class="text-xl sm:text-xl font-semibold text-center sm:text-left leading-relaxed mb-4 sm:mb-0">
+              Gavali Group of Business is a diversified investment firm
+              specializing in stock trading and real estate, offering
+              comprehensive wealth management solutions. With expertise in
+              equity markets and property investments, we help individuals,
+              corporations, and institutions maximize returns and diversify
+              portfolios.
+            </h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
-=======
-    <div className='flex item items-center bg-black text-white'>Home</div>
-  )
-}
->>>>>>> bd57f26971f9cc1376deb1163e97361b34ea32e9:Client/src/Components/Users/Home.jsx
 
 export default Home;
