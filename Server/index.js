a8343b0480e29a86be5d2242df5fdcd0452215d3
Vaibhav <vck3333@gmail.com>
2025-02-27 12:13:24 +0530

    deployment testing

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 const corsOptions ={
-    origin:'http://localhost:5173', 
+    origin:'https://gavali-groups.vercel.app', 
     credentials:true,  
     optionSuccessStatus:200
 }
